f1fb8bfcf7054e34f24f2d1190fa937479285efc
animesh <animeshchaudhri3@gmail.com>
2024-04-11 03:17:22 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Mystery struct {
+	ID         string  `json:"id"`
+	Name       string  `json:"Name"`
+	Role       string  `json:"Role"`
+	Popularity float64 `json:"Popularity"`
+}
+
+var Gravity_falls = []Mystery{
+	{ID: "1", Name: "Dipper Pines", Role: "Protagonist", Popularity: 56.99},
+	{ID: "2", Name: "Mabel Pines", Role: "Protagonist", Popularity: 17.99},
+	{ID: "3", Name: "Bill Cipher", Role: "Antagonist", Popularity: 39.99},
+}
+
+func getMysterys(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Gravity_falls)
+}
+func main() {
+	router := gin.Default()
+	router.GET("/Mysterys", getMysterys)
+
+	router.Run("localhost:8080")
+}
