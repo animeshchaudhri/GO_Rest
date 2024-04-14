d17c5f84d68b9f354cea31e6c5516ca0cf31a922
animesh <animeshchaudhri3@gmail.com>
2024-04-14 23:29:01 +0530

    fix: fixed typo

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type Mystery struct {
 var Gravity_falls = []Mystery{
 	{ID: "1", Name: "Dipper Pines", Role: "Protagonist", Popularity: 56.99},
 	{ID: "2", Name: "Mabel Pines", Role: "Protagonist", Popularity: 17.99},
-	{ID: "3", Name: "Bill Cipher", Role: "Antagonist", Popularity: 39.99},
+	{ID: "3", Name: "Bill Cipher", Role: "Antagonist", Popularity: 40.99},
 }
 
 func getMysterys(c *gin.Context) {
